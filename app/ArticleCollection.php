271d507ec0c3aab9cbab12f2cf013db8f9258b62
Jānis Gitendorfs <janis.gitendorfs@gmail.com>
2023-11-10 19:02:58 +0200

    Changes

--- app/ArticleCollection.php
+++ app/ArticleCollection.php
@@ -5,47 +5,50 @@ namespace App;
 
 use App\Models\Article;
 use Carbon\Carbon;
+
 class ArticleCollection
 {
     private const API_KEY = "1d109792a10a4c41939f87200bd0ba08";
-    public function getSearchedArticles(string $topic, ?string $from=null, ?string $to=null ): array
+
+    public function getSearchedArticles(string $topic, ?string $from = null, ?string $to = null): array
     {
-        if($from==null)$from=(Carbon::yesterday())->format("Y-m-d");
-        if($to==null)$to=(Carbon::now())->format("Y-m-d");
-        $api="https://newsapi.org/v2/everything?q=$topic&from=$from&to=$to&sortBy=popularity&apiKey=".self::API_KEY;
+        if ($from == null) $from = (Carbon::yesterday())->format("Y-m-d");
+        if ($to == null) $to = (Carbon::now())->format("Y-m-d");
+        $api = "https://newsapi.org/v2/everything?q=$topic&from=$from&to=$to&sortBy=popularity&apiKey=" . self::API_KEY;
         $client = new \GuzzleHttp\Client();
-        $req=$client->get($api);
-        $response =json_decode($req->getBody()->getContents());
+        $req = $client->get($api);
+        $response = json_decode($req->getBody()->getContents());
         $articles = [];
         foreach ($response->articles as $article) {
-                $articles[] = new Article(
-                    $article->title,
-                    $article->url,
-                    $article->publishedAt,
-                    $article->urlToImage
-                );
+            $articles[] = new Article(
+                $article->title,
+                $article->url,
+                $article->publishedAt,
+                $article->urlToImage
+            );
         }
         return $articles;
 
     }
-    public function getHeadlines(string $country):array
-    {
-        $api="https://newsapi.org/v2/top-headlines?country=$country&apiKey=".self::API_KEY;
-        $client = new \GuzzleHttp\Client();
-        $req=$client->get($api);
-        $response =json_decode($req->getBody()->getContents());
-        $articles = [];
-        foreach ($response->articles as $article){
-                $articles[] = new Article(
-                    $article->title,
-                    $article->url,
-                    $article->publishedAt,
-                    $article->urlToImage
 
-                );
+    public function getHeadlines(string $country): array
+    {
+        $api = "https://newsapi.org/v2/top-headlines?country=$country&apiKey=" . self::API_KEY;
+        $client = new \GuzzleHttp\Client();
+        $req = $client->get($api);
+        $response = json_decode($req->getBody()->getContents());
+        $articles = [];
+        foreach ($response->articles as $article) {
+            $articles[] = new Article(
+                $article->title,
+                $article->url,
+                $article->publishedAt,
+                $article->urlToImage
+
+            );
         }
 
-        return  $articles;
+        return $articles;
     }
 
 }
